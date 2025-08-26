03c61900b308eeccc2081d55d1a5b43476c9180a
dasogu92 <sorianoyecla@gmail.com>
2025-08-26 14:05:48 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,19 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-white to-gray-100 flex flex-col items-center justify-start p-6 space-y-6 font-poppins text-red-600">
       <header className="w-full flex justify-between items-center px-4 py-4">
-        <Image src="/logo-manitas.svg" alt="Logo" width={140} height={40} />
-        <Image src="/icon-hamburger.svg" alt="Menu" width={30} height={30} />
+        <Image src="/LOGO TEXTO.svg" alt="Logo texto" width={140} height={40} />
+        <Image src="/BURGER.svg" alt="Menu" width={30} height={30} />
       </header>
 
       <main className="flex flex-col items-center space-y-6 mt-6">
         <div className="relative">
-          <Image src="/user1.svg" alt="Soy un manitas" width={120} height={120} className="rounded-full" />
+          <Image src="/TRABAJADOR.svg" alt="Soy un manitas" width={120} height={120} className="rounded-full" />
           <div className="absolute -top-6 left-1/2 -translate-x-1/2 bg-white px-3 py-1 rounded-full shadow text-xs font-bold">
             ¡Soy un manitas!
           </div>
         </div>
         <div className="relative">
-          <Image src="/user2.svg" alt="Necesito un manitas" width={120} height={120} className="rounded-full" />
+          <Image src="/CLIENTE.svg" alt="Necesito un manitas" width={120} height={120} className="rounded-full" />
           <div className="absolute -top-6 left-1/2 -translate-x-1/2 bg-white px-3 py-1 rounded-full shadow text-xs font-bold">
             ¡Necesito un manitas!
           </div>
